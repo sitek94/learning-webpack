bf126802bcd9ae1af4aed1bccad180971770b348
Maciek Sitkowski <msitkowski94@gmail.com>
2020-09-07 15:19:25 +0200

    Cache Busting, Html Webpack Plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,21 @@
 // We don't have to install path - it comes with node
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'main-[contentHash].js',
     // __dirname - absolute path
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html"
+    })
+  ],
   module: {
     rules: [
       {
